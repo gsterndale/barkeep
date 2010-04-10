95be1d2f027c8d25e1d414af76114eb77d681214
Greg Sterndale <gsterndale@gmail.com>
2010-04-10 14:58:25 -0400

    Refactor martini_spec ingredients example to use expect{}.to change{}.from().to()

--- spec/martini_spec.rb
+++ spec/martini_spec.rb
@@ -1,15 +1,16 @@
 require 'spec/spec_helper'
 
 describe Barkeep::Martini do
+  let(:mixer) { 'vermouth' }
   it_should_behave_like "a drink"
   it { should have_attribute(:garnish) }
   it { should have_attribute(:mixer) }
-end
-
-describe Barkeep::Martini, "with a mixer" do
-  let(:mixer) { 'vermouth' }
-  subject { Barkeep::Martini.new(:mixer => mixer) }
-  its(:ingredients) { should include mixer }
-  its(:ingredients) { should have(1).items }
+  it "should include mixer in ingredients" do
+    expect { subject.mixer = mixer }.
+    to
+    change { subject.ingredients }.
+    from( [] ).
+    to [ mixer ]
+  end
 end
 
