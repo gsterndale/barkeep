d2d38f08f3536fe0b1c6a0f01ba978b394a06f99
Greg Sterndale <gsterndale@gmail.com>
2010-04-10 09:36:47 -0400

    Spec helper only servers to require my library, spec itself and the files in spec/support/

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,8 @@
-$LOAD_PATH.unshift(File.dirname(__FILE__))
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'barkeep'
 require 'spec'
-require 'spec/autorun'
 
-Spec::Runner.configure do |config|
-  
-end
+# Requires supporting files with custom matchers and macros, etc,
+# in ./support/ and its subdirectories.
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
+
+
