fb49cf822c891cc6d3b3a5e10bce487f1e3dd965
Greg Sterndale <gsterndale@gmail.com>
2014-07-15 05:02:54 +0200

    Verify partial doubles

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,3 +3,9 @@ require 'barkeep'
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
+
+RSpec.configure do |config|
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+end
