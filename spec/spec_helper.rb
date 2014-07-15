7b69e0523df148e77271f8915f1314e51e37e6d5
Greg Sterndale <gsterndale@gmail.com>
2014-07-15 21:45:48 +0200

    Filter runs by focus tag

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,9 @@ require 'barkeep'
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
 
 RSpec.configure do |config|
+  config.filter_run focus: true
+  config.run_all_when_everything_filtered = true
+
   config.mock_with :rspec do |mocks|
     mocks.verify_partial_doubles = true
   end
