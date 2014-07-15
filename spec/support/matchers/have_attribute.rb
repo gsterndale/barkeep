d8293a1f2941d73250747ed2f601ebc3d01f915b
Greg Sterndale <gsterndale@gmail.com>
2014-07-15 05:04:05 +0200

    Refactor custom matcher

--- spec/support/matchers/have_attribute.rb
+++ spec/support/matchers/have_attribute.rb
@@ -1,77 +1,24 @@
 module AttributeMatchers
-
-  class HaveAttribute
-
-    def initialize(expected, writer=true, reader=true)
-      @expected               = expected
-      @reader                 = reader
-      @writer                 = writer
-      @names_not_responded_to = []
-      @names_responded_to     = []
-      @names                  = []
-      @names << getter if !!reader
-      @names << setter if !!writer
-      super()
+  RSpec::Matchers.define :have_readable_attribute do |name|
+    match do |target|
+      getter = name.to_sym
+      target.respond_to?(getter)
     end
-
-    def description
-      "have a #{attribute_adjectives} attribute named #{@expected}"
-    end
-
-    def matches?(target)
-      @target = target
-      @names.each do |name|
-        if @target.respond_to?(name)
-          @names_responded_to << name
-        else
-          @names_not_responded_to << name
-        end
-      end
-      @names_not_responded_to.empty?
-    end
-
-    def failure_message
-      "expected #{@target.inspect} to respond to " <<
-        @names_not_responded_to.collect {|name| name.inspect }.join(', ')
-    end
-
-    def failure_message_when_negated
-      "expected #{@target.inspect} not to respond to " <<
-        @names_responded_to.collect {|name| name.inspect }.join(', ')
-    end
-
-    def getter
-      @expected
-    end
-
-    def setter
-      (getter.to_s+'=').to_sym
-    end
-
-    def attribute_adjectives
-      adjectives = []
-      adjectives << 'readable' if @reader
-      adjectives << 'writable' if @writer
-      adjectives.join(' and ')
-    end
-
   end
 
-  def have_attribute(expected)
-    HaveAttribute.new(expected)
+  RSpec::Matchers.define :have_writable_attribute do |name|
+    match do |target|
+      setter = (name.to_s + "=").to_sym
+      target.respond_to?(setter)
+    end
   end
-  alias_method :have_attribute_named, :have_attribute
 
-  def have_readable_attribute(expected)
-    HaveAttribute.new(expected, false, true)
+  RSpec::Matchers.define :have_attribute do |name|
+    match do |target|
+      expect(target).to have_readable_attribute(name) &
+        have_writable_attribute(name)
+    end
   end
-  alias_method :have_readable_attribute_named, :have_readable_attribute
-
-  def have_writable_attribute(expected)
-    HaveAttribute.new(expected, true, false)
-  end
-  alias_method :have_writable_attribute_named, :have_writable_attribute
-
 end
 
 RSpec.configure do |config|
