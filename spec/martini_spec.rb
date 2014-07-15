068e315e1bcf151b1979c1894cb26a3e95b979dd
Greg Sterndale <gsterndale@gmail.com>
2014-07-15 21:49:58 +0200

    Enumerable, compound and composite matchers!

--- spec/martini_spec.rb
+++ spec/martini_spec.rb
@@ -28,3 +28,23 @@ describe Barkeep::Martini, ".dirty" do
   subject { Barkeep::Martini.dirty }
   it_behaves_like "a drink", "vodka", "vermouth"
 end
+
+describe Barkeep::Martini, ".dirty" do
+  let(:martini)         { Barkeep::Martini.dirty }
+  subject(:ingredients) { martini.ingredients }
+
+  it { is_expected.to contain_exactly 'vodka', 'olive juice' }
+
+  it { is_expected.to include 'olive juice', 'vodka' }
+
+  it { is_expected.to all be_a(String) & start_with(/^\w/) }
+
+  it { is_expected.to match [ start_with(/^\w/), match(/olives?|twist/) ]}
+
+  it "names two ingredients, the second olive(s) or a lemon twist" do
+    expect(ingredients).to match [
+       a_string_starting_with(/^\w/),
+       a_string_matching(/olives?|twist/)
+    ]
+  end
+end
